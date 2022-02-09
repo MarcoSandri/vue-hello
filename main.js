3f0be87c4133f10cc0c44408059377a2039fe8a7
Marco Sandri <sandrimarco98@gmail.com>
2022-02-09 15:09:45 +0100

    Test vue

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ var app = new Vue (
         el: '#root',
         data: {
             messaggio : "Hello World",
-            src : "https://picsum.photos/200/300"
+            src : "https://picsum.photos/50",
+            side: 50
+        },
+        methods: {
+            changeImage() {
+                this.side += 50;
+                this.src = `https://picsum.photos/${this.side}`;
+            },
         }
     }
 );
